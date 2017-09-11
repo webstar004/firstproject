b179a31f93c02d82b58f6aa07426973c4ca0e8a2
sliver <sliver@gmail.com>
2017-09-11 04:25:56 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,63 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>click demo</title>
+  <style>
+  p {
+    color: red;
+    margin: 5px;
+    cursor: pointer;
+  }
+  p:hover {
+    background: yellow;
+  }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+ 
+<p>First Paragraph</p>
+<p>Second Paragraph</p>
+<p>Yet one more Paragraph</p>
+ 
+<script>
+$( "p" ).click(function() {
+  $( this ).slideUp();
+});
+</script>
+ 
+</body>
+</html>
